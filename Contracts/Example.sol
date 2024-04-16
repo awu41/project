73c2d7e18fd36c24f5d840c8bed3154be26369aa
awu41 <champwume@gmail.com>
2024-04-16 09:08:11 -0400

    wrote the skeleton code for feed

--- Contracts/Example.sol
+++ Contracts/Example.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Example {
+contract BoilerGotchi {
     uint256 public value;
 
     event ValueChanged(uint256 newValue);
@@ -14,5 +14,10 @@ contract Example {
     function getValue() public view returns (uint256) {
         return value;
     }
+
+	function feed() public {
+		// Feed the gotchi and raise the energy level and happiness value
+		// Require gotchi exist and its energy isn't full
+	}
 }
 
