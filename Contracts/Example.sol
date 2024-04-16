334799129e2f44234d117090c524b3fb5c41aee3
azeng24 <zengannie81437@gmail.com>
2024-04-16 16:37:32 -0400

    added function to check if pet is suicidal and added to getMood

--- Contracts/Example.sol
+++ Contracts/Example.sol
@@ -7,19 +7,38 @@ contract BoilerGotchi {
 		uint256 mood;
 		uint256 energy;
 		address owner;
+		uint256 lastPlayed;
 	}
-    
+
+	mapping(uint256 => Gotchi) public gotchis; 
+
 	function feed() public {
 		// Feed the gotchi and raise the energy level and happiness value
 		// Require gotchi exist and its energy isn't full
 	}
 
-	// Get the mood of a BoilerGotchi using the name
-	function getMood (string name) public {
-	}
+	function checkSuicidal(uint256 id) internal {
+        Gotchi storage g = gotchis[id];
+        if (block.timestamp - g.lastPlayed > 365 days) {
+            g.mood = -1; // suicidal mood
+        }
+    }
 
-	// Set the mood of a BoilerGotchi using the name
-	function setMood (string name) public {
+    function getMood(uint256 id) public view returns (string memory) {
+		//first check to see if gotchi is suicidal
+		checkSuicidal(id);
+		Gotchi storage g = gotchis[id];
+        if (g.mood >= 200) { // Happy if mood has score 200+
+            return "Happy";
+        }
+		else if (g.mood >= 100) { // Chill if mood has score 100 - 199
+            return "Chill";
+        }
+		else if (g.mood >= 1) { // Sad if mood has score 0-99
+            return "Sad";
+        }
+		else {
+            return "Suicidal"; // Suicidal if mood has score -1 (hasn't been played or fed in 365 days)
+        }
 	}
 }
-
