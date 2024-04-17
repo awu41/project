01aff867115305c72b08a0191dcb6253851026aa
azeng24 <zengannie81437@gmail.com>
2024-04-16 20:04:42 -0400

    move mood code from Example.sol to initialize.sol

--- Contracts/initalize.sol
+++ Contracts/initalize.sol
@@ -76,4 +76,39 @@ contract boilerGotchi is ERC721 {
         g.energy = energy;
     }
 
+	function checkSuicidal(uint256 id) internal {
+        Gotchi storage g = gotchis[id];
+        if (block.timestamp - g.lastPlayed > 365 days) {
+            g.mood = -1; // suicidal mood
+        }
+    }
+
+	function getMood(uint256 id) public view returns (string memory) {
+		//first check to see if gotchi is suicidal
+		checkSuicidal(id);
+		Gotchi storage g = gotchis[id];
+        if (g.mood >= 200) { // Happy if mood has score 200+
+            return "Happy";
+        }
+		else if (g.mood >= 100) { // Chill if mood has score 100 - 199
+            return "Chill";
+        }
+		else if (g.mood >= 1) { // Sad if mood has score 0-99
+            return "Sad";
+        }
+		else {
+            return "Suicidal"; // Suicidal if mood has score -1 (hasn't been played or fed in 365 days)
+        }
+	}
+
+	function checkStatus(uint256 id) public view returns (string memory) {
+		require(msg.sender == ownerOf(id), "Caller is not the owner.");
+        Gotchi storage g = gotchis[id];
+		//check if gotchi is suicidal and update mood
+        checkSuicidal(id);
+		//get the mood of gotchi
+        string memory moodDescription = getMood(id);
+        return string(abi.encodePacked("Name: ", g.name, "\nMood: ", moodDescription, "\nEnergy: ", uint256ToString(g.energy)));
+    }
+
 }
